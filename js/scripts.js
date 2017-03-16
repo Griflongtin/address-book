b9ed66a24a4d9c1e18b75d8c1ba1f759fdbb2739
Griffin P Longtin and Joshua Fairchild <griffin@>
2017-03-15 18:05:22 -0700

    this will add an address constructor to the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 //Business Logic
-
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
